7ff7b475b67a8963ebf9900b8f4c0a043745b541
Leo Füreder <leo.fuereder@outlook.com>
2024-09-26 11:10:58 +0200

    Added List of strings with forEach lambda

--- Calculator/src/main/java/net/austrianlinuxmemer/calculator/HalloJavaMitForEach.java
+++ Calculator/src/main/java/net/austrianlinuxmemer/calculator/HalloJavaMitForEach.java
@@ -1,4 +1,10 @@
 package net.austrianlinuxmemer.calculator;
 
+import java.util.List;
+
 public class HalloJavaMitForEach {
+    public static void main(String[] args) {
+        List<String> strings = List.of(args);
+        strings.forEach((String s) -> System.out.println(s));
+    }
 }
