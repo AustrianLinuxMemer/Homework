2a80725fdd6f769e7496c1d3258c070d3564802d
Leo Füreder <leo.fuereder@outlook.com>
2024-09-26 11:16:53 +0200

    Added PrimeTester

--- Calculator/src/main/java/net/austrianlinuxmemer/calculator/NumberTester.java
+++ Calculator/src/main/java/net/austrianlinuxmemer/calculator/NumberTester.java
@@ -2,10 +2,14 @@ package net.austrianlinuxmemer.calculator;
 
 public class NumberTester {
     NumberTest oddTester;
+    NumberTest primeTester;
     public NumberTester(String fileName) {
 
     }
     public void setOddEvenTester(NumberTest oddTester) {
         this.oddTester = oddTester;
+    }
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
     }
 }
