7c83318f3c21b4dd828b703c79eb29b883f32bc3
Leo Füreder <leo.fuereder@outlook.com>
2024-09-26 11:15:54 +0200

    Added OddEvenTester

--- Calculator/src/main/java/net/austrianlinuxmemer/calculator/NumberTester.java
+++ Calculator/src/main/java/net/austrianlinuxmemer/calculator/NumberTester.java
@@ -1,7 +1,11 @@
 package net.austrianlinuxmemer.calculator;
 
 public class NumberTester {
+    NumberTest oddTester;
     public NumberTester(String fileName) {
-        
+
+    }
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
     }
 }
