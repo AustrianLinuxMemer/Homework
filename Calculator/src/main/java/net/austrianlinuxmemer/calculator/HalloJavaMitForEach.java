339197ee77fef7a76163d99bf40375e712748f5d
Leo Füreder <leo.fuereder@outlook.com>
2024-09-26 11:11:55 +0200

    Added Consumer anonymous implemtation

--- Calculator/src/main/java/net/austrianlinuxmemer/calculator/HalloJavaMitForEach.java
+++ Calculator/src/main/java/net/austrianlinuxmemer/calculator/HalloJavaMitForEach.java
@@ -1,10 +1,17 @@
 package net.austrianlinuxmemer.calculator;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class HalloJavaMitForEach {
     public static void main(String[] args) {
         List<String> strings = List.of(args);
         strings.forEach((String s) -> System.out.println(s));
+        strings.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
     }
 }
